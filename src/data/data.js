25bb907c0d60de0fe43ae35f37db6de4ce60c5b4
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-09-17 20:11:29 -0300

    v1.0

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+
+export const data = [
+    {
+        id: 1,
+        img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
+        title: "Entregable Front",
+        desc: "",
+        link: "",
+        git: "",
+    },
+    {
+        id: 2,
+        img: 'https://i.postimg.cc/dQxcsZ56/To-Do.jpg',
+        title: "ToDo-App Initial Template",
+        link: "",
+        git: "https://github.com/Gu1ll3rm04r13l/ToDo-App-template-inicial",
+    },
+    {
+        id: 3,
+        img: 'https://i.postimg.cc/k4SjyLv2/work1.jpg',
+        title: "Final Frontend",
+        desc: "",
+        link: "",
+        git: "https://github.com/Gu1ll3rm04r13l/FinalFront1",
+    }
+
+];
