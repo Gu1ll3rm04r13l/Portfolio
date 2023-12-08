f0e5b63cd7f43065f83ff9c45365519e46332ea1
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-12-08 18:21:13 -0300

    fix

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 
-import ff3 from '../assets/ff3.png'
-import ff2 from '../assets/ff2.png'
+import ff3 from "../assets/ff3.png"
+import ff2 from "../assets/ff2.png"
 import ff1 from "../assets/ff1.png"
 
 export const data = [
